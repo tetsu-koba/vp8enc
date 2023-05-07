df2175d379744a7d5044633f3915e7943c83b128
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2023-05-07 22:58:11 +0900

    Split to vp8enc.zig

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,9 @@
 const std = @import("std");
-const c = @cImport({
-    @cInclude("vpx/vpx_encoder.h");
-    @cInclude("vpx/vp8cx.h");
-});
 const IVF = @import("ivf.zig");
-
-const FILE = std.os.FILE;
+const VP8Enc = @import("vp8enc.zig").VP8Enc;
 
 pub fn main() !void {
-    //const allocator = std.heap.page_allocator;
+    const alc = std.heap.page_allocator;
 
     var yuv_file = try std.fs.cwd().openFile("input.yuv", .{});
     defer yuv_file.close();
@@ -19,7 +14,8 @@ pub fn main() !void {
     const width: u32 = 160;
     const height: u32 = 120;
     const bitrate: u32 = 1000;
-    const fps: u32 = 15;
+    const framerate: u32 = 15;
+    const time_scale: u32 = 1;
     const keyframe_interval: u32 = 60;
 
     const ivf_header = IVF.IVFHeader{
@@ -29,81 +25,28 @@ pub fn main() !void {
         .fourcc = .{ 'V', 'P', '8', '0' }, //"VP80",
         .width = width,
         .height = height,
-        .frame_rate = fps,
-        .time_scale = 1,
+        .frame_rate = framerate,
+        .time_scale = time_scale,
         .num_frames = 0,
         .unused = 0,
     };
     var ivf_writer = try IVF.IVFWriter.init(outfile, &ivf_header);
     defer ivf_writer.deinit();
 
-    var cfg: c.vpx_codec_enc_cfg_t = undefined;
-    if (c.vpx_codec_enc_config_default(c.vpx_codec_vp8_cx(), &cfg, 0) != 0) {
-        std.debug.print("Error getting default configuration\n", .{});
-        return error.DefaultConfigurationError;
-    }
+    const yuv_size = width * height * 3 / 2;
+    var yuv_buf = try alc.alloc(u8, yuv_size);
+    defer alc.free(yuv_buf);
 
-    cfg.rc_target_bitrate = bitrate;
-    cfg.g_w = width;
-    cfg.g_h = height;
-    cfg.g_timebase.num = 1;
-    cfg.g_timebase.den = fps;
-    cfg.g_error_resilient = 1;
-    cfg.kf_min_dist = keyframe_interval;
-    cfg.kf_max_dist = keyframe_interval;
-
-    var codec: c.vpx_codec_ctx_t = undefined;
-    if (c.vpx_codec_enc_init(&codec, c.vpx_codec_vp8_cx(), &cfg, 0) != 0) {
-        std.debug.print("Error initializing codec\n", .{});
-        return error.CodecInitializationError;
-    }
-    defer _ = c.vpx_codec_destroy(&codec);
-
-    var raw = c.vpx_img_alloc(null, c.VPX_IMG_FMT_I420, width, height, 1);
-    defer _ = c.vpx_img_free(raw);
+    var vp8enc = try VP8Enc.init(width, height, framerate, time_scale, bitrate, keyframe_interval, yuv_buf);
+    defer vp8enc.deinit();
 
     var frame_count: u32 = 0;
     while (true) {
-        if (!try read_yuv(raw, yuv_file)) {
+        if (yuv_size != try yuv_file.readAll(yuv_buf)) {
             break;
         }
-
-        if (c.vpx_codec_encode(&codec, raw, frame_count, 1, 0, c.VPX_DL_REALTIME) != 0) {
-            std.debug.print("Error encoding frame\n", .{});
-            return error.EncodingError;
-        }
-
-        var iter: ?*usize = null;
-        while (true) {
-            const pkt = c.vpx_codec_get_cx_data(&codec, @ptrCast([*c]?*const anyopaque, &iter));
-            if (pkt == null) break;
-
-            const p = pkt.*;
-            if (p.kind != c.VPX_CODEC_CX_FRAME_PKT) {
-                continue;
-            }
-            const frame_size = p.data.frame.sz;
-            // If you need keyframe
-            // const keyframe = (p.data.frame.flags & c.VPX_FRAME_IS_KEY) != 0;
-            try ivf_writer.writeIVFFrame(@ptrCast([*]const u8, p.data.frame.buf)[0..frame_size], frame_count);
-            frame_count += 1;
-        }
+        const buf = try vp8enc.encode(frame_count);
+        try ivf_writer.writeIVFFrame(buf, frame_count);
+        frame_count += 1;
     }
-}
-
-fn read_yuv(raw: *c.struct_vpx_image, yuv_file: std.fs.File) !bool {
-    const r = raw.*;
-    const y_size = @intCast(usize, r.stride[0]) * @intCast(usize, r.d_h);
-    const u_size = @intCast(usize, r.stride[1]) * @intCast(usize, r.d_h) / 2;
-    const v_size = @intCast(usize, r.stride[2]) * @intCast(usize, r.d_h) / 2;
-    if (y_size != try yuv_file.readAll(@ptrCast([*]u8, r.planes[0])[0..y_size])) {
-        return false;
-    }
-    if (u_size != try yuv_file.readAll(@ptrCast([*]u8, r.planes[1])[0..u_size])) {
-        return false;
-    }
-    if (v_size != try yuv_file.readAll(@ptrCast([*]u8, r.planes[2])[0..v_size])) {
-        return false;
-    }
-    return true;
 }
---
+++ src/vp8enc.zig
@@ -0,0 +1,87 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("vpx/vpx_encoder.h");
+    @cInclude("vpx/vp8cx.h");
+});
+
+pub const VP8Enc = struct {
+    width: u32,
+    height: u32,
+    framerate: u32,
+    time_scale: u32,
+    bitrate: u32,
+    keyframe_interval: u32,
+    codec: c.vpx_codec_ctx_t,
+    img: c.struct_vpx_image,
+
+    const Self = @This();
+
+    pub fn init(
+        width: u32,
+        height: u32,
+        framerate: u32,
+        time_scale: u32,
+        bitrate: u32,
+        keyframe_interval: u32,
+        yuv_buf: []u8,
+    ) !VP8Enc {
+        var self = VP8Enc{
+            .width = width,
+            .height = height,
+            .framerate = framerate,
+            .time_scale = time_scale,
+            .bitrate = bitrate,
+            .keyframe_interval = keyframe_interval,
+            .codec = undefined,
+            .img = undefined,
+        };
+        var cfg: c.vpx_codec_enc_cfg_t = undefined;
+        if (c.vpx_codec_enc_config_default(c.vpx_codec_vp8_cx(), &cfg, 0) != 0) {
+            std.debug.print("Error getting default configuration\n", .{});
+            return error.DefaultConfigurationError;
+        }
+
+        cfg.rc_target_bitrate = bitrate;
+        cfg.g_w = width;
+        cfg.g_h = height;
+        cfg.g_timebase.num = 1;
+        cfg.g_timebase.den = @intCast(c_int, framerate);
+        cfg.g_error_resilient = 1;
+        cfg.kf_min_dist = keyframe_interval;
+        cfg.kf_max_dist = keyframe_interval;
+
+        if (c.vpx_codec_enc_init(&self.codec, c.vpx_codec_vp8_cx(), &cfg, 0) != 0) {
+            std.debug.print("Error initializing codec\n", .{});
+            return error.CodecInitializationError;
+        }
+        _ = c.vpx_img_wrap(&self.img, c.VPX_IMG_FMT_I420, width, height, 1, @ptrCast([*c]u8, yuv_buf));
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        _ = c.vpx_codec_destroy(&self.codec);
+    }
+
+    pub fn encode(self: *Self, frame_count: u32) ![]const u8 {
+        if (c.vpx_codec_encode(&self.codec, &self.img, frame_count, 1, 0, c.VPX_DL_REALTIME) != 0) {
+            std.debug.print("Error encoding frame\n", .{});
+            return error.EncodingError;
+        }
+
+        var iter: ?*usize = null;
+        while (true) {
+            const pkt = c.vpx_codec_get_cx_data(&self.codec, @ptrCast([*c]?*const anyopaque, &iter));
+            if (pkt == null) break;
+
+            const p = pkt.*;
+            if (p.kind != c.VPX_CODEC_CX_FRAME_PKT) {
+                continue;
+            }
+            const frame_size = p.data.frame.sz;
+            // If you need keyframe
+            // const keyframe = (p.data.frame.flags & c.VPX_FRAME_IS_KEY) != 0;
+            return @ptrCast([*]const u8, p.data.frame.buf)[0..frame_size];
+        }
+        unreachable;
+    }
+};
