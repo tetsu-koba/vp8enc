fe92e000af68aa109714835049ae3d757c926083
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2025-03-03 15:32:52 +0900

    Test on macos

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -eux
+case "$OSTYPE" in
+    darwin*) case "$HOSTTYPE" in
+		 arm64) LIBDIR=/opt/homebrew/lib;INCLUDE=/opt/homebrew/include;;
+		 *)	LIBDIR=/usr/local/lib;INCLUDE=/usr/local/include;;
+	     esac ;;
+     *) LIBDIR=/usr/lib;INCLUDE=/usr/include;;
+esac
+
 if [ $# -eq 0 ]; then
     OPTS=-Doptimize=Debug
 else
@@ -6,6 +14,6 @@ else
 fi
 zig build
 for i in src/*_test.zig; do
-    zig test $OPTS $i -lvpx -lc
+    zig test $OPTS $i -I$INCLUDE -L$LIBDIR -lvpx -lc
 done
 
